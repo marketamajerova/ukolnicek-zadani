a7a6bce8b78249bfa6e553237946d4617fb60898
marketamajerova <marketamajerova.6@gmail.com>
2022-10-20 20:33:13 +0200

    Adding component Task

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('funguju!');
+const Task = (props) => {
+    return `
+        <div class="task">
+            <div class="task__body">
+            <div class="task__name">${props.name}</div>
+            <div class="task__due">${props.due}</div>
+            </div>
+            <div class="task__done">${props.due}</div>
+        </div> `
+}
+
+
